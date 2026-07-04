525cf677a3160963c84efaf5b0c326c86ea83c02
wotjq2 <wotjq2@gmail.com>
2026-07-04 10:33:16 +0900

    Render 배포 URL 설정

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8000/api'
+const API_URL = import.meta.env.VITE_API_URL || 'https://csep-kpxv.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_URL,
